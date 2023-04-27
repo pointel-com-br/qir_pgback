4aa3030c9b4f912a763be0dbeb1f95b8e3af0196
Éverton M. Vieira <emuvi@outlook.com.br>
2023-04-27 07:53:51 -0300

    always ask the restore information

--- restore.py
+++ restore.py
@@ -6,20 +6,22 @@ import utils
 
 
 class Restore:
+    origin_data_group = "periodically"
     origin_backup_week = "(now::week)"
     target_restore_host = "localhost"
-    data_group_to = "periodically"
+    
 
     def __init__(self,
-                 target_restore_host: str = "",
+                 origin_data_group: str = "",
                  origin_backup_week: str = "",
-                 data_group_to: str = ""):
-        if target_restore_host:
-            self.target_restore_host = target_restore_host
+                 target_restore_host: str = ""):
+        if origin_data_group:
+            self.origin_data_group = origin_data_group
         if origin_backup_week:
             self.origin_backup_week = origin_backup_week
-        if data_group_to:
-            self.data_group_to = data_group_to
+        if target_restore_host:
+            self.target_restore_host = target_restore_host
+        
 
     def get_target_week(self) -> str:
         if self.origin_backup_week == "(now::week)":
@@ -30,7 +32,7 @@ class Restore:
 
 def restore_globals(restore: Restore):
     globals_name = utils.get_data_path(
-        restore.data_group_to,
+        restore.origin_data_group,
         "globals-" + restore.get_target_week() + ".bkp")
     if os.system("psql -h " + restore.target_restore_host +
                  " -U postgres -f " + globals_name) == 0:
@@ -59,7 +61,7 @@ def restore_database(restore: Restore, path_name):
 
 def restore_globals_and_databases(restore: Restore):
     restore_globals(restore)
-    origin_path = utils.get_data_folder(restore.data_group_to)
+    origin_path = utils.get_data_folder(restore.origin_data_group)
     for inside_path in os.listdir(origin_path):
         if inside_path.startswith("db-") and inside_path.endswith(
                 "-" + restore.get_target_week() + ".bkp"):
@@ -69,19 +71,13 @@ def restore_globals_and_databases(restore: Restore):
 
 if __name__ == "__main__":
     print("Restore Globals and Databases")
-    host = os.getenv("QIR_PGBACK_HOST", "")
-    if not host :
-        host = input("Host: ")
-    week = os.getenv("QIR_PGBACK_WEEK", "")
-    if not week:
-        week = input("Week: ")
-    group = os.getenv("QIR_PGBACK_GROUP", "")
-    if not group:
-        group = input("Group: ")
-    restore = Restore(host, week, group)
+    host = input("Host: ")
+    week = input("Week: (Monday == 0 ... Sunday == 6)")
+    group = input("Group: (periodically | emergency)")
+    restore = Restore(group, week, host)
     confirm = input(
         "Do you wanna restore the globals and all databases?\n" +
-        "  From data: '" + restore.data_group_to + "'\n" +
+        "  From group: '" + restore.origin_data_group + "'\n" +
         "  And of week: '" + restore.get_target_week() + "'\n" +
         "  And to host: '" + restore.target_restore_host + "' ? (y/N) : ")
     if confirm != "y":
