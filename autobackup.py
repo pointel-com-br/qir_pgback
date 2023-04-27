d99518931a69f2de1ff9eecb2e123e80877033e7
Éverton M. Vieira <emuvi@outlook.com.br>
2023-04-27 09:08:30 -0300

    retrieves the host from the enviroment variable

--- autobackup.py
+++ autobackup.py
@@ -1,3 +1,4 @@
+import os
 import time
 from datetime import datetime
 
@@ -13,13 +14,16 @@ def backup_periodically():
 
 
 if __name__ == "__main__":
+    env_host = os.getenv("QIR_PGBACK_HOST", "")
+    if env_host:
+        backup_of_host = env_host
     minutes_passed = 300
     while True:
         now_str = datetime.now().strftime("%Y-%m-%d %H-%M")
         if minutes_passed >= 300:
             minutes_passed = 0
-            print(now_str + 
-                " - Have passed five hours so we must to do the periodically backup.")
+            print(now_str +
+                  " - Have passed five hours so we must to do the periodically backup.")
             backup_periodically()
         time.sleep(60)
         minutes_passed += 1
